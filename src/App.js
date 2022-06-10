38f1cc1eec67216df4df84fddce3e0b01313f765
Manjula <mguneratne3@gmail.com>
2022-06-10 11:29:26 -0400

    flex box framework added

--- src/App.js
+++ src/App.js
@@ -2,27 +2,13 @@ import "./App.css";
 
 function App() {
   return (
-    <div class="row">
-
-      <div class="column left">
-        <h2>Main Content</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit
-          amet pretium urna. Vivamus venenatis velit nec neque ultricies, eget
-          elementum magna tristique. Quisque vehicula, risus eget aliquam
-          placerat, purus leo tincidunt eros, eget luctus quam orci in velit.
-          Praesent scelerisque tortor sed accumsan convallis.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit
-          amet pretium urna. Vivamus venenatis velit nec neque ultricies, eget
-          elementum magna tristique. Quisque vehicula, risus eget aliquam
-          placerat, purus leo tincidunt eros, eget luctus quam orci in velit.
-          Praesent scelerisque tortor sed accumsan convallis.
-        </p>
+    <div className="flex-container">
+      <div className="flex-left">
+        <h2>Colour Cycle App</h2>
+        <p className="box"></p>
       </div>
 
-      <div class="column right">
+      <div className="flex-right">
         <h2>Side</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit..</p>
       </div>
