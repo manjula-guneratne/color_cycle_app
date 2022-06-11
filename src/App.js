b7be83017db7bb281549f9c9150730d22e8bdb41
Manjula <mguneratne3@gmail.com>
2022-06-10 21:54:14 -0400

    useState add and all input values collected

--- src/App.js
+++ src/App.js
@@ -1,6 +1,32 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [colorValue, setColorValue] = useState({
+    firstColorValues: "00",
+    secondColorValues: "00",
+    thirdColorValues: "00",
+    colorIncRate: "00",
+    colorChangeRate: "00"
+  });
+
+  const getInputValue = (e) => {
+    const value = e.target.value;
+    setColorValue({
+      ...colorValue,
+      [e.target.name]: value
+    })
+  }
+
+  let handleClick = () => {
+    console.log(`ALL THE VALUESS...... 
+    firstColorValues: + ${colorValue.firstColorValues}
+    secondColorValues: + ${colorValue.secondColorValues}
+    thirdColorValues: + ${colorValue.thirdColorValues}
+    colorIncRate: + ${colorValue.colorIncRate}
+    colorChangeRate: + ${colorValue.colorChangeRate}`);
+  };
+
   return (
     <div className="flex-container">
       <div className="flex-left">
@@ -8,23 +34,25 @@ function App() {
         <p className="box"></p>
       </div>
       <div className="flex-right">
-        <h3>Starting Hex color breakdown</h3>
+        <h4>Starting Hex color breakdown</h4>
         <span className="right_lable">No.1 and 2 </span>
-        <input type="text" placeholder="##" maxLength={2}></input>
+        <input type="text" placeholder="00" maxLength={2} name="firstColorValues" value={colorValue.firstColorValues} onChange={getInputValue}></input>
         <br />
         <span className="right_lable">No.3 and 4 </span>
-        <input type="text" placeholder="##" maxLength={2}></input>
+        <input type="text" placeholder="#00" maxLength={2} name="secondColorValues" value={colorValue.secondColorValues} onChange={getInputValue}></input>
         <br />
         <span className="right_lable">No.5 and 6 </span>
-        <input type="text" placeholder="##" maxLength={2}></input>
+        <input type="text" placeholder="00" maxLength={2} name="thirdColorValues" value={colorValue.thirdColorValues} onChange={getInputValue}></input>
+        <br />
         <br />
         <span className="right_lable">Hex color inc. rate </span>
-        <input type="text" placeholder="#######"></input>
+        <input type="text" placeholder="#######" name="colorIncRate" value={colorValue.colorIncRate} onChange={getInputValue}></input>
         <br />
         <span className="right_lable">Hex color changing rate </span>
-        <input type="text" placeholder="#######"></input>
+        <input type="text" placeholder="#######" name="colorChangeRate" value={colorValue.colorChangeRate} onChange={getInputValue}></input>
         <br />
-        <button>Start / Stop</button>
+        <br />
+        <button onClick={handleClick}>Start / Stop</button>
       </div>
     </div>
   );
